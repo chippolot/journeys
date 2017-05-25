67d771d7463eaebddc64436a890567c02b347609
Ben Smith <ben@z2.com>
2017-05-25 08:00:13 -0700

    Blank project

---
+++ Journeys/Journeys/JourneysView.h
@@ -0,0 +1,13 @@
+//
+//  JourneysView.h
+//  Journeys
+//
+//  Created by Ben Smith on 5/25/17.
+//  Copyright © 2017 Ben Smith. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface JourneysView : ScreenSaverView
+
+@end
---
+++ Journeys/Journeys/JourneysView.m
@@ -0,0 +1,52 @@
+//
+//  JourneysView.m
+//  Journeys
+//
+//  Created by Ben Smith on 5/25/17.
+//  Copyright © 2017 Ben Smith. All rights reserved.
+//
+
+#import "JourneysView.h"
+
+@implementation JourneysView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
